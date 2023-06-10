3805c27110d8560aa99ab8c88ba6ba077a7fa953
TienQuyen <tienquyen307@gmail.com>
2023-06-10 11:00:59 +0700

    add index.ejs file

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Node.js');
-}).listen(3000, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:3000');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const database = require("./src/database");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log("server is running...");
+})
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// session
+const session = require("express-session");
+app.use(
+    session({
+        resave: true,
+        saveUninitialized:true,
+        secret: process.env.SESSION_SECRET,
+        cookie: {
+            maxAge: 3600000, // miliseconds
+            secure: false
+        }
+    })
+);
+
+
+app.get("/",function(req,res){
+    const Product = require("./src/models/product");
+    Product.find({})
+        .then(rs=>{
+            res.render("home",{
+                products: rs
+            })
+        })
+        .catch(err=>{
+            res.send(err);      
+        })
+
+})
---
+++ src/database.js
@@ -0,0 +1,19 @@
+const server = process.env.DB_HOST;
+const db_name = process.env.DB_NAME;
+const mongoose = require("mongoose");
+class Database{
+    constructor(){
+        this._connect();
+    }
+    _connect(){
+        mongoose.connect(`${server}/${db_name}`)
+        .then(()=>{  // call back function
+            console.log("Connect successfully");
+        })
+        .catch(err=>{
+            console.log(err);
+        });
+    }
+}
+
+module.exports = new Database();
